be213f89af3141e0c15f2dfa52c7cbc9971a406e
Igo Medeiros <igopdm@gmail.com>
2020-06-05 06:45:59 -0300

    Add routes on server && create seed, migrate && database

--- server/src/database/migrations/00_create_points.ts
+++ server/src/database/migrations/00_create_points.ts
@@ -2,7 +2,7 @@ import Knex from 'knex';
 
 export async function up(knex: Knex) {
     return knex.schema.createTable('points', table => {
-        table.increments('index').primary();
+        table.increments('id').primary();
         table.string('image').notNullable();
         table.string('name').notNullable();
         table.string('email').notNullable();
